5faea1713dd3dab199b68eaf98464186d88caf44
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-08 13:08:15 +0300

    Day0 submitted

---
+++ src/main/java/org/sajourney/HackerRankDrills/monthdrill/SolutionDay0.java
@@ -0,0 +1,20 @@
+package org.sajourney.HackerRankDrills.monthdrill;
+
+import java.util.*;
+
+public class SolutionDay0 {
+        public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN.
+        Print output to STDOUT.
+        Your class should be named Solution. */
+
+            Scanner scan = new Scanner(System.in);
+            String inputString = scan.nextLine();
+
+            System.out.println("Hello, World.");
+            System.out.println(inputString);
+            scan.close();
+
+    }
+
+}
